8db09cb66d95e2367858690beb613911afe7d942
Dmitri Ranfft <ranfft.dmitri@gmail.com>
2022-12-04 16:27:07 +0100

    Add script that deletes the symlinks from the server folder

---
+++ delete_symlinks.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+rm ~/valheim_server/BepInEx
+rm ~/valheim_server/doorstop_config.ini
+rm ~/valheim_server/doorstop_libs
+rm ~/valheim_server/start_server_bepinex.sh
+rm ~/valheim_server/unstripped_corlib
+rm ~/valheim_server/winhttp.dll
