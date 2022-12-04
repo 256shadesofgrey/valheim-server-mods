c2c45734009789b2eecac6cda129ee381e44827b
Dmitri Ranfft <ranfft.dmitri@gmail.com>
2022-12-04 16:25:06 +0100

    Add script that creates links in the server folder to enable mods

---
+++ create_symlinks.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+ln -s ~/mods/BepInEx/                ~/valheim_server/BepInEx
+ln -s ~/mods/doorstop_config.ini     ~/valheim_server/doorstop_config.ini
+ln -s ~/mods/doorstop_libs/          ~/valheim_server/doorstop_libs
+ln -s ~/mods/start_server_bepinex.sh ~/valheim_server/start_server_bepinex.sh
+ln -s ~/mods/unstripped_corlib/      ~/valheim_server/unstripped_corlib
+ln -s ~/mods/winhttp.dll             ~/valheim_server/winhttp.dll
